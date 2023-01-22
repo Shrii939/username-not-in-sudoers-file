efd0865c6605a0bafacb23d501ca6730ac99b85c
Shridhar <70104162+Shrii939@users.noreply.github.com>
2023-01-22 11:48:21 +0530

    Update and rename wow.sh to mkAdmin.sh

--- mkAdmin.sh
+++ mkAdmin.sh
@@ -2,9 +2,9 @@
 
 # to run this script u need to be in the location where this file is downloaded 
 # open the terminal and run this command 
-# chmod +x wow.sh 
+# chmod +x mkAdmin.sh 
 # and then type
-# ./wow.sh
+# ./mkAdmin.sh
 
 # continuation of this.sh
 echo -e "\tready....opening the file in 3 " 
